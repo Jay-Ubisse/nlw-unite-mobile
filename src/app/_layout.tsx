d170c1c3b0fa13a296be3da347948eb6dd76d9de
Jay-Ubisse <joaquimubisse@gmail.com>
2024-04-03 23:40:22 +0200

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "../styles/global.css";
+
+export default function Layout() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Slot />
+    </>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="bg-red-500">Home Page</Text>
+    </View>
+  );
+};
+
+export default Home;
